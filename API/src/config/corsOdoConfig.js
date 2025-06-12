1b4e7378470722d285ce719ab947a690a7cdacc7
CARLITOSSS24 <carlitosojeda59@gmail.com>
2025-06-11 21:06:34 -0500

    Subo proyecto

--- API/src/config/corsOdoConfig.js
+++ API/src/config/corsOdoConfig.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: ['https://odontomedicalsoft.vercel.app', 'http://localhost:5173'],
+    origin: ['https://odomedical.vercel.app/', 'http://localhost:5173'],
     methods: 'GET,POST,PATCH,DELETE',
     allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true
